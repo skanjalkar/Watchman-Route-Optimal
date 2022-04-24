ee60c5f4524886bcbd8d4c7822c97767a4b9fd30
Rutwik Bonde <rrbonde@wpi.edu>
2022-04-24 15:32:09 -0400

    Tested the polygon with holes

--- Sampling_Planning_Main.py
+++ Sampling_Planning_Main.py
@@ -48,18 +48,21 @@ if __name__ == "__main__":
     # points = [(120.04002564611195, 26.748405823643653), (1.8319882514537322, 8.381256634659618), (4.475299130777356, 8.0519414026726), (3.447646309306825, 6.967555834038162)]
     # points = [(88,134),(133,173),(99,191),(261,377),(88,134)]  # 4 guards polygon - zigzag
     # points = [(105,58),(180,476)] # mega one
-    points = [(63,159),(121,220),(273,324),(63,159)]
+    # points = [(63,159),(121,220),(273,324),(63,159)]
     # print(points)
     # points = [(70,275),(300,445),(70,275)]
-    map_array = load_map("D:\Educational\A WPI Assignments and Materials\Motion Planning\Project\Colored Polygons\GS3.jpeg",1)
+    points = [(147,130),(228,90),(265,322),(125,265),(147,130)] # Polygon with holes
+    map_array = load_map("D:\Educational\A WPI Assignments and Materials\Motion Planning\Project\Colored Polygons\PH_BW1.png",1)
     for i in range(len(points)-1):
         start = points[i]
         goal = points[i+1]
         RRT_planner = RRT(map_array, start, goal)
-        # RRT_planner.RRT(n_pts=1000)
-        # RRT_planner.RRT_star(n_pts=2000)
-        RRT_planner.informed_RRT_star(n_pts=4000)
+        RRT_planner.RRT(n_pts=4000)
+        # RRT_planner.RRT_star(n_pts=4000)
+        # RRT_planner.informed_RRT_star(n_pts=6000)
     plt.show()
+    
+    
     # # Planning class
     # RRT_planner = RRT(map_array, start, goal)
     # # Search with RRT and RRT*
